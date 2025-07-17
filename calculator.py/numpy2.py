ba20923a7539f6d4c130024f6f000e8c072eb074
aviraj54 <avirajmishra544@gmail.com>
2025-07-17 17:18:20 +0545

    numpy part 2

---
+++ calculator.py/numpy2.py
@@ -0,0 +1,8 @@
+import numpy as np
+myarray=np.array([[2,3,3],[1,1,1],[2,2,2]])
+print(myarray.dtype)
+print(myarray.shape,myarray.size)
+zarray=np.zeros((3,3))
+print(zarray)
+a=np.arange(21)
+print(a)
